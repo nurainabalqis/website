a96a6a36a04c9d9d193bf756beeffd7f667184a1
Nur Aina Balqis <148413115+nurainabalqis@users.noreply.github.com>
2025-05-19 08:32:43 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+/*****************************************************
+* Author of the page:                                *
+* Nur Aina Balqis Binti Mohamad Zaparin              *
+* Matric No : A23CS0151                              *
+* Section : 10                                       *
+*****************************************************/
+
+document.addEventListener("DOMContentLoaded", () => {
+    const ctaButton = document.querySelector(".cta");
+    ctaButton.addEventListener("click", () => {
+        window.location.href = "projects.html";
+    });
+});
